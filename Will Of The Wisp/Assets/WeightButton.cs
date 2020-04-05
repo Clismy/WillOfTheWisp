3c5114386d26f447def566a30590cd15b0252767
Christos <christos.kalanidis@hotmail.com>
2020-04-05 20:34:41 +0200

    Added Alpha on Weight Buttons
    Weight buttons now are a little faded if they are not activated, to show more clearly if they are on or not

--- Will Of The Wisp/Assets/WeightButton.cs
+++ Will Of The Wisp/Assets/WeightButton.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WeightButton : MonoBehaviour
 {
@@ -9,17 +10,26 @@ public class WeightButton : MonoBehaviour
 
     bool activated = false;
 
+    // Used so we can change the alpha of the button, to more clearly see if it is activated or not.
+    Image buttonImage;
+    [SerializeField] Color activatedButtonColor;
+    [SerializeField] Color deActivatedButtonColor;
+    
      public Scale scale;
 
     // Start is called before the first frame update
     void Start()
     {
+        buttonImage = GetComponent<Image>();
+
         activated = false;
+        buttonImage.color = deActivatedButtonColor;
     }
 
     public void InteractLever()
     {
         activated = !activated;
+        buttonImage.color = activated ? activatedButtonColor : deActivatedButtonColor;
 
         int amountToSend = activated ? amount : -amount;
         scale.GetAmount(amountToSend);
