3481979b8ea39222b3cec4a679cfef13debd4f53
Peter <zorkan.p@gmail.com>
2020-04-05 19:19:32 +0200

    Weight Puzzel
    Added a simple weight puzzel

---
+++ Will Of The Wisp/Assets/Scale.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scale : MonoBehaviour
+{
+    public int counterWeight;
+    public int maxAngle;
+    private int amountToSend;
+    private int weightAmount;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.rotation = counterWeight > maxAngle ?  Quaternion.Euler(0, 0, maxAngle) : Quaternion.Euler(0, 0, counterWeight);
+        
+    }
+
+    public void GetAmount(int amountToSend)
+    {
+        weightAmount -= amountToSend;
+
+        int finalWeight = counterWeight + weightAmount;
+
+        Debug.Log(finalWeight);
+        transform.rotation = finalWeight > maxAngle ? Quaternion.Euler(0, 0, maxAngle) : Quaternion.Euler(0, 0, finalWeight);
+        if (finalWeight == 0)
+        {
+            Debug.Log("Congratulations");
+        }
+
+    }
+}
---
+++ Will Of The Wisp/Assets/WeightButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeightButton : MonoBehaviour
+{
+    [Range(0, 100)]
+    [SerializeField] int amount;
+
+    bool activated = false;
+
+     public Scale scale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        activated = false;
+    }
+
+    public void InteractLever()
+    {
+        activated = !activated;
+
+        int amountToSend = activated ? amount : -amount;
+        scale.GetAmount(amountToSend);
+    }
+
+    public void DisableLever()
+    {
+        activated = false;
+    }
+}
